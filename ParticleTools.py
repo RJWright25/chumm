feca4d0228930cfc356c58c0450f6b7d5002a2e2
Ruby Wright <21486778@student.uwa.edu.au>
2019-07-14 09:14:01 +0800

    Added eagle mass function

--- ParticleTools.py
+++ ParticleTools.py
@@ -7,6 +7,8 @@
 
 import numpy as np
 import h5py
+import read_eagle
+import pandas.DataFrame as df
 
 def read_n_part(run_directory,sim_type='SWIFT',snap_prefix="snap_",snap_lz=4):
 
@@ -56,7 +58,7 @@ def read_n_part(run_directory,sim_type='SWIFT',snap_prefix="snap_",snap_lz=4):
         print('Please enter valid simulation string.')
         return []
 
-########################### READ MASS DATA ###########################
+########################### READ MASS TABLE ###########################
 
 def read_mass_table(run_directory,sim_type='SWIFT',snap_prefix="snap_",snap_lz=4):
 
@@ -106,3 +108,65 @@ def read_mass_table(run_directory,sim_type='SWIFT',snap_prefix="snap_",snap_lz=4
     else:
         print('Please enter valid simulation string.')
         return []
+
+########################### READ MASS DATA (EAGLE) ###########################
+
+def read_mass_data_eagle(fname,extra_gas_props): 
+    
+    """
+
+    get_mass_data_eagle : function
+	----------
+
+    Grabs the gas and dark matter masses (and any extra gas properties) for EAGLE particles in a given snapshot. 
+
+    Parameters
+	----------
+
+    fname : str
+        The base filename of the EAGLE snapshot file to be loaded. 
+
+    Returns
+	----------
+
+    mass_table : list
+        mass_table[0] : pd.DataFrame of gas particle IDs ['IDs'] and corresponding masses ['Mass'] (and any extra gas_props)
+        mass_table[1] : float dark matter particle mass in sim units
+
+    
+    """
+
+    snap = read_eagle.EagleSnapshot(fname)
+    snap.select_region(xmin=0,xmax=snap.boxsize,ymin=0,ymax=snap.boxsize,zmin=0,zmax=snap.boxsize)
+
+    print ("# Total number of gas particles in snapshot = %d" % snap.numpart_total[0])
+    print ("# Total number of DM particles in snapshot = %d" % snap.numpart_total[1])
+
+    Gas_Props={}
+    Gas_Props["IDs"]=snap.read_dataset(0,"ParticleIDs")
+    Gas_Props["Masses"]=snap.read_dataset(0,"Mass")
+
+    for prop in extra_gas_props:
+        Gas_Props[prop]=snap.read_dataset(0,prop)
+
+    Gas_Props=df(Gas_Props)
+    
+    fh5py=h5py.File(fname)
+    h=fh5py["Header"].attrs.get("HubbleParam")
+    a=fh5py["Header"].attrs.get("Time")
+    dm_mass=fh5py["Header"].attrs.get("MassTable")[1]
+    cgs=fh5py['PartType0/Mass'].attrs.get("CGSConversionFactor")
+    aexp=fh5py['PartType0/Mass'].attrs.get("aexp-scale-exponent")
+    hexp=fh5py['PartType0/Mass'].attrs.get("h-scale-exponent")
+
+    Msun_cgs=np.float(units.M_sun.cgs.scale)
+    DM_Mass=dm_mass*cgs*a**aexp*h**hexp/Msun_cgs
+
+    DM_Props={}
+    DM_Props['Masses']=DM_Mass
+
+    mass_table=[Gas_Props,DM_Props]
+    return mass_table
+
+
+    
