308a4c386665651c8ca23aee8c319234b54fb5c9
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 08:42:31 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -198,6 +198,7 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
         L2_Processed_Particles_FRESH_Types=np.concatenate([Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices])
         L2_Processed_Particles_FRESH=df(np.column_stack((L2_Processed_Particles_FRESH_IDs,L2_Processed_Particles_FRESH_Types)),dtype=int,columns=['ParticleID','ParticleType']).sort_values(['ParticleType','ParticleID'])
         print(L2_Processed_Particles_FRESH.iloc[0:100])
+        print(L2_Processed_Particles_FRESH.iloc[-100:1])
         t2=time.time()
         print(f'Finished finding particles in structure in {t2-t1} sec')
 
