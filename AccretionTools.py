9153b0d2b3e90762b1a8dd20de37879cad2ed8a2
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 17:00:06 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1223,7 +1223,7 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
     
                 ipart_switch=ipart_switch+1
                 if ipart_switch%10000==0:
-                    print(ipart_switch/len(fieldhalo_Particles_bytype[str(itype)])*100,'% done flipping L1&L2 switches')
+                    print(ipart_switch/len(fieldhalo_Particles_bytype[str(itype)])*100,f'% done flipping L1&L2 switches for {PartNames[itype]} particles')
 
                 sorted_index_temp_ID_L1=binary_search_2(element=temp_ID_L1,sorted_array=Particle_History_Flags[str(itype)]["ParticleIDs_Sorted"])
                 Particle_History_Flags[str(itype)]["HostStructure"][sorted_index_temp_ID_L1]=1
