c98f4e3180f8ec360115fe8c67602062277f25e7
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 21:41:15 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1239,8 +1239,6 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
             t2=time.time()
             print(f"Added host halos in {t2-t1} sec for {PartNames[itype]} particles")
 
-            for i in range(10000):
-                print(np.array(Particle_History_Flags[str(itype)]["HostHaloIndex"][i]))
 
         print(f'Dumping data to file')
         t1=time.time()
