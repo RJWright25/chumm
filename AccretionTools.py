9e0559fa3ccadd569dac9f26d43782ea053c6d86
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 12:17:57 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1230,7 +1230,7 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
                         print(sorted_index_at_prev,sorted_index_at_now)
                         print(Particle_History_Flags_PrevSnap[str(itype)]["ParticleIDs_Sorted"][index_at_prev],temp_itype_ParticleID)
             else:
-                print('Not checking previous state given this is the first snap we have data for")
+                print('Not checking previous state given this is the first snap we have data for')
 
         isnap+=1
 
