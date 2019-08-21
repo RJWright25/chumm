7774e9721289d437e663960c94cc88c6ae521bca
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 18:20:38 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1184,21 +1184,17 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
         
         n_halos=len(snap_Halo_Particle_Lists["Particle_IDs"])
         n_halo_particles=[len(snap_Halo_Particle_Lists["Particle_IDs"][ihalo]) for ihalo in range(n_halos)]
-
         temp_subhalo_indices=np.where(base_halo_data[snap]["hostHaloID"]>0)[0]
         temp_field_indices=np.where(base_halo_data[snap]["hostHaloID"]<0)[0]
-        
-        fieldhalo_Particle_hosts=np.concatenate([np.ones(n_halo_particles[ihalo],dtype='uint32')*ihalo for ihalo in range(n_halos)])
-        subhalo_Particle_hosts=np.concatenate([np.ones(n_halo_particles[ihalo])*ihalo for ihalo in temp_subhalo_indices])
+        allhalo_Particle_hosts=np.concatenate([np.ones(n_halo_particles[ihalo],dtype='uint32')*ihalo for ihalo in range(n_halos)])
+        subhalo_Particle_hosts=np.concatenate([np.ones(n_halo_particles[ihalo],dtype='uint32')*ihalo for ihalo in temp_subhalo_indices])
         
         #fieldhalo==l1, subhalo==l2
-        fieldhalo_Particles=df({'ParticleIDs':np.concatenate(snap_Halo_Particle_Lists['Particle_IDs']),'ParticleTypes':np.concatenate(snap_Halo_Particle_Lists['Particle_Types'])},dtype=int).sort_values(["ParticleIDs"])
-        subhalo_Particles=df({'ParticleIDs':np.concatenate([snap_Halo_Particle_Lists['Particle_IDs'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),'ParticleTypes':np.concatenate([snap_Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices])},dtype=int).sort_values(["ParticleIDs"])
+        fieldhalo_Particles=df({'ParticleIDs':np.concatenate(snap_Halo_Particle_Lists['Particle_IDs']),'ParticleTypes':np.concatenate(snap_Halo_Particle_Lists['Particle_Types']),"HostHaloIndex":allhalo_Particle_hosts},dtype=int).sort_values(["ParticleIDs"])
+        subhalo_Particles=df({'ParticleIDs':np.concatenate([snap_Halo_Particle_Lists['Particle_IDs'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),'ParticleTypes':np.concatenate([snap_Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),"HostHaloIndex":subhalo_Particle_hosts},dtype=int).sort_values(["ParticleIDs"])
             
         fieldhalo_Particles_bytype={str(itype):np.array(fieldhalo_Particles["ParticleIDs"].loc[fieldhalo_Particles["ParticleTypes"]==itype]) for itype in PartTypes}
         subhalo_Particles_bytype={str(itype):np.array(subhalo_Particles["ParticleIDs"].loc[subhalo_Particles["ParticleTypes"]==itype]) for itype in PartTypes}
-
-       
         
         t2=time.time()
         print(f"Loaded, concatenated and sorted halo particle lists in {t2-t1} sec")
