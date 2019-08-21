7646fec5564e1ae33f9d6632b4aab2c34bc3b806
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 18:07:42 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1184,10 +1184,11 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
         
         n_halos=len(snap_Halo_Particle_Lists["Particle_IDs"])
         n_halo_particles=[len(snap_Halo_Particle_Lists["Particle_IDs"][ihalo]) for ihalo in range(n_halos)]
-        print(n_halos)
-        print(n_halo_particles[0:10])
         temp_subhalo_indices=np.where(base_halo_data[snap]["hostHaloID"]>0)[0]
         
+        fieldhalo_Particle_hosts=np.concatenate([np.ones(n_halo_particles[ihalo])*base_halo_data[snap]["hostHaloID"][ihalo] for ihalo in range(n_halos)])
+        print(len(fieldhalo_Particle_hosts))
+
         #fieldhalo==l1, subhalo==l2
         fieldhalo_Particles=df({'ParticleIDs':np.concatenate(snap_Halo_Particle_Lists['Particle_IDs']),'ParticleTypes':np.concatenate(snap_Halo_Particle_Lists['Particle_Types'])},dtype=int).sort_values(["ParticleIDs"])
         subhalo_Particles=df({'ParticleIDs':np.concatenate([snap_Halo_Particle_Lists['Particle_IDs'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),'ParticleTypes':np.concatenate([snap_Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices])},dtype=int).sort_values(["ParticleIDs"])
