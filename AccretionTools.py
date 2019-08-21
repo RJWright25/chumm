8a384059681f2e6f24c438cac7e68b325eef50cc
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 12:25:59 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1218,11 +1218,13 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
             Particle_History_Flags[str(itype)]={"ParticleIDs_Sorted":np.sort(Particle_IDs_Unsorted_itype),"ParticleIndex_Original":np.argsort(Particle_IDs_Unsorted_itype),"Processed_L1":np.zeros(N_Particles_itype),"Processed_L2":np.zeros(N_Particles_itype)}
             t2=time.time()
 
-            print(f"Mapped IDs to indices for all {PartNames[itype]} particles at snap {snap} in {t2-t1}")
+            print(f"Mapped IDs to indices for all {PartNames[itype]} particles at snap {snap} in {t2-t1} sec")
             
             #check if existed previously & add data
             if isnap>0:
                 for sorted_index_at_now,temp_itype_ParticleID in enumerate(Particle_History_Flags[str(itype)]['ParticleIDs_Sorted']):
+                    if sorted_index_at_now%1000==0:
+                        print(sorted_index_at_now/N_Particles_itype*100,f'% done checking previous {PartNames[itype]} particles')
                     sorted_index_at_prev=np.searchsorted(Particle_History_Flags_PrevSnap[str(itype)]["ParticleIDs_Sorted"],temp_itype_ParticleID)#index of this ID from current snap in prev sorted list
                     if Particle_History_Flags_PrevSnap[str(itype)]["ParticleIDs_Sorted"][sorted_index_at_prev]==temp_itype_ParticleID:
                         pass
