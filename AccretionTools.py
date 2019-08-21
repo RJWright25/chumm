023d8cccd859757d1e4fe481445893dccad9708a
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 18:04:46 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1191,9 +1191,7 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
         #fieldhalo==l1, subhalo==l2
         fieldhalo_Particles=df({'ParticleIDs':np.concatenate(snap_Halo_Particle_Lists['Particle_IDs']),'ParticleTypes':np.concatenate(snap_Halo_Particle_Lists['Particle_Types'])},dtype=int).sort_values(["ParticleIDs"])
         subhalo_Particles=df({'ParticleIDs':np.concatenate([snap_Halo_Particle_Lists['Particle_IDs'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),'ParticleTypes':np.concatenate([snap_Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices])},dtype=int).sort_values(["ParticleIDs"])
-        
-        print(np.column_stack((fieldhalo_Particles["ParticleIDs"][5000:5200],fieldhalo_Particles["hostHaloID"][5000:5200])))
-        
+            
         fieldhalo_Particles_bytype={str(itype):np.array(fieldhalo_Particles["ParticleIDs"].loc[fieldhalo_Particles["ParticleTypes"]==itype]) for itype in PartTypes}
         subhalo_Particles_bytype={str(itype):np.array(subhalo_Particles["ParticleIDs"].loc[subhalo_Particles["ParticleTypes"]==itype]) for itype in PartTypes}
         
