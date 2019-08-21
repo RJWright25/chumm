76391fd5270c61dabb73f44c11307251d5b86dd8
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-21 11:19:20 +0800

    Small changes to gen_particle_histories

--- AccretionTools.py
+++ AccretionTools.py
@@ -1193,7 +1193,7 @@ def gen_particle_history_serial(base_halo_data,snaps=[],verbose=1):
         fieldhalo_Particles=df({'ParticleIDs':np.concatenate(snap_Halo_Particle_Lists['Particle_IDs']),'ParticleTypes':np.concatenate(snap_Halo_Particle_Lists['Particle_Types'])},dtype=int).sort_values(["ParticleIDs"])
         subhalo_Particles=df({'ParticleIDs':np.concatenate([snap_Halo_Particle_Lists['Particle_IDs'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices]),'Particle_Types':np.concatenate([snap_Halo_Particle_Lists['Particle_Types'][temp_subhalo_index] for temp_subhalo_index in temp_subhalo_indices])},dtype=int).sort_values(["ParticleIDs"])
         
-        print(np.array(fieldhalo_Particles['ParticleTypes']))
+        print(np.array(fieldhalo_Particles['ParticleTypes'])==0)
 
         fieldhalo_Particles_bytype=[fieldhalo_Particles.iloc[np.array(fieldhalo_Particles['ParticleTypes'])==itype] for itype in PartTypes]
         subhalo_Particles_bytype=[fieldhalo_Particles.iloc[np.array(subhalo_Particles['ParticleTypes'])==itype] for itype in PartTypes]
