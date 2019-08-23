16feccf82eead29016b50861ba8bbc1eab3f7281
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-23 18:27:49 +0800

    Playing with find_progen_index

--- STFTools.py
+++ STFTools.py
@@ -539,7 +539,7 @@ def find_progen_index(base_halo_data,index2,snap2,snap1): ### given halo index2 
     index_idepth=index2
     for idepth in range(depth):
         current_ID=base_halo_data[snap2-idepth]["ID"][index_idepth]
-        tail_ID=base_halo_data[snap2-idepth]["Tail"][index_idepth][0]
+        tail_ID=base_halo_data[snap2-idepth]["Tail"][index_idepth]
         index_idepth=np.where(base_halo_data[snap2-idepth-1]["ID"]==tail_ID)[0][0]
     
     return index_idepth
