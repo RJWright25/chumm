9de15f8a0e90ec104261e3abd0813be8203371b6
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-23 11:26:33 +0800

    Small changes to gen_particle_histories

--- STFTools.py
+++ STFTools.py
@@ -157,6 +157,7 @@ def gen_base_halo_data(partdata_filelist,partdata_filetype,vr_filelist,vr_filety
         if item["ID"][0]<temporal_idval:
             #read in IDs from TreeFrog
             treefile_compressed_isnap=tf_filelist[isnap]
+            print(treefile_compressed_isnap)
             treefrog_ids=h5py.File(treefile_compressed_isnap)["ID"]
             halo_data_all[isnap]["ID"]=treefrog_ids
             print(treefrog_ids)
