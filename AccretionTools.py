c10efbc3789c62d71382eb6513b70882dc305c5a
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-26 17:06:54 +0800

    Playing with AccretionTools

--- AccretionTools.py
+++ AccretionTools.py
@@ -954,7 +954,7 @@ def gen_accretion_data_serial(base_halo_data,snap=None,test_run=False,halo_index
                     new_particle_IDs_itype_snap2_historyindex=binary_search_1(sorted_array=Part_Histories_IDs_snap2[iitype],elements=new_particle_IDs_itype_snap2)
                     new_particle_IDs_itype_snap1_historyindex=binary_search_1(sorted_array=Part_Histories_IDs_snap1[iitype],elements=new_particle_IDs_itype_snap2)
                     #particle_masses
-                    new_particle_masses=[snap_2_masses[str(itype)][Part_Histories_Index_snap2[str(itype)][history_index]] for history_index in new_particle_IDs_itype_snap2_historyindex]
+                    new_particle_masses=[snap_2_masses[str(itype)][Part_Histories_Index_snap2[iitype][history_index]] for history_index in new_particle_IDs_itype_snap2_historyindex]
                     #pre-processed
                     previous_hostIDs=[Part_Histories_HostStructure_snap1[history_index] for history_index in new_particle_IDs_itype_snap1_historyindex]
         else:
