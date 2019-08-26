89ada4f5cb17b2a34a0b5a39333880201d1f266a
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-26 11:26:43 +0800

    Playing with AccretionTools

--- AccretionTools.py
+++ AccretionTools.py
@@ -934,7 +934,8 @@ def gen_accretion_data_serial(base_halo_data,snap=None,test_run=False,halo_index
 
                 print(f"Finding index of accreted particles in halo {ihalo_s2} of type {itype}: n = {len(new_particle_IDs_itype_snap2)}")
                 new_particle_IDs_itype_snap2_historyindex=binary_search_1(sorted_array=Part_Histories_IDs_snap2[iitype],elements=new_particle_IDs_itype_snap2)
-                print(np.sum(new_particle_IDs_itype_snap2_historyindex==np.nan))
+                new_particle_IDs_itype_snap1_historyindex=binary_search_1(sorted_array=Part_Histories_IDs_snap1[iitype],elements=new_particle_IDs_itype_snap2)
+                print(np.sum(new_particle_IDs_itype_snap1_historyindex==np.nan))
         else:
             #### return nan accretion rate
 
