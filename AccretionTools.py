2f3fa9f0ca3d6b886d1d5a8d7f3cb6c78b6add09
Ruby Wright <21486778@student.uwa.edu.au>
2019-08-29 05:21:46 +0800

    Playing with AccretionTools

--- AccretionTools.py
+++ AccretionTools.py
@@ -461,7 +461,7 @@ def collate_acc_data(directory):
     
     acc_data_hdf5files=[h5py.File('acc_data/'+acc_data_file,'r') for acc_data_file in acc_data_filelist]
     acc_data_hdf5files_header=acc_data_hdf5files[0]['Header']
-    print(list(acc_data_hdf5files_header.attrs))
+    print(acc_data_hdf5files_header.attrs)
 
 
 
