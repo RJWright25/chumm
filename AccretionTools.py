44af399f499da77aaa1a2f082887b9e62d2811e0
Ruby Wright <21486778@student.uwa.edu.au>
2019-09-05 16:26:39 +0800

    Adding function to read ihalo particle accretion data

--- AccretionTools.py
+++ AccretionTools.py
@@ -781,10 +781,10 @@ def get_particle_acc_data(snap,halo_index_list):
     else:
         halo_index_list=list(halo_index_list)
 
-    directory='acc_data/snap_'+str(snap).zfill(3)
+    directory='acc_data/snap_'+str(snap).zfill(3)+'/'
     accdata_filelist=os.listdir(directory)
-    accdata_filelist_trunc=sorted([accfile for accfile in accdata_filelist if (('summed' not in accfile) and ('px' not in accfile))])
-    accdata_files=[h5py.File(accdata_file) for accdata_file in accdata_filelist_trunc]
+    accdata_filelist_trunc=sorted([directory+accfile for accfile in accdata_filelist if (('summed' not in accfile) and ('px' not in accfile))])
+    accdata_files=[h5py.File(accdata_filename,'r') for accdata_filename in accdata_filelist_trunc]
     accdata_halo_lists=[list(accdata_file.keys()) for accdata_file in accdata_files]
     print(accdata_halo_lists)
     desired_num_halos=len(halo_index_list)
