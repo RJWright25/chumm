83e05bdb35e41f936d263b996829ca2cbbd15e1d
Ruby Wright <21486778@student.uwa.edu.au>
2019-10-08 12:08:45 +0800

    Adding outflow functionality to AccretionTools

--- AccretionTools.py
+++ AccretionTools.py
@@ -621,10 +621,10 @@ def gen_accretion_data_serial(base_halo_data,snap=None,halo_index_list=None,pre_
                     new_particle_IDs_itype_snap1_historyindex=[]
                     for new_ID in new_particle_IDs_itype_snap2:
                         if not itype==4:#don't need to check presence of particles
-                            snap2_index=bisect_left(Part_Histories_IDs_snap2[iitype],new_ID,lo=0,hi=None)
-                            snap1_index=bisect_left(Part_Histories_IDs_snap1[iitype],new_ID,lo=0,hi=None)
+                            snap2_index=bisect_left(Part_Histories_IDs_snap2[iitype],new_ID,lo=0,hi=new_particle_count)
+                            snap1_index=bisect_left(Part_Histories_IDs_snap1[iitype],new_ID,lo=0,hi=new_particle_count)
                         else:
-                            snap2_index=bisect_left(Part_Histories_IDs_snap2[iitype],new_ID,lo=0,hi=None)#must be in this list
+                            snap2_index=bisect_left(Part_Histories_IDs_snap2[iitype],new_ID,lo=0,hi=new_particle_count)#must be in this list
                             snap1_index=binary_search_2(sorted_array=Part_Histories_IDs_snap1[iitype],element=new_ID)
                         if not snap1_index>-10:
                             lost=lost+1
