2a6c74793a83e9b041f1f8609abcf397a51cc971
Ruby Wright <21486778@student.uwa.edu.au>
2019-10-24 14:19:04 +0800

    Cleaning up file handling

--- AccretionTools.py
+++ AccretionTools.py
@@ -1352,6 +1352,7 @@ def add_gas_particle_data(base_halo_data,accdata_path,datasets=None):
 
         gas_IDs_in_snap1=acc_file[ihalo_group]['Inflow']['PartType0']['ParticleIDs']
         gas_IDs_out_snap1=acc_file[ihalo_group]['Outflow']['PartType0']['ParticleIDs']
+        print(type(gas_IDs_in_snap1))
         
         if np.size(gas_IDs_in_snap1)==1 and type(gas_IDs_in_snap1)==float:
             for dataset in datasets:
