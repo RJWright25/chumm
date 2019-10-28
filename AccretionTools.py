1c0306b47ab0fb758c2a55b379e9251ff7c519e7
Ruby Wright <21486778@student.uwa.edu.au>
2019-10-28 15:35:54 +0800

    Cleaning up file handling

--- AccretionTools.py
+++ AccretionTools.py
@@ -1529,10 +1529,10 @@ def add_gas_particle_data(base_halo_data,accdata_path,datasets=None):
             ihalo_datasets_outflow[f'snap1_{dataset}']=np.array(ihalo_datasets_outflow[f'snap1_{dataset}'],dtype=np.float32)
             
             if dataset_shapes[dataset]>1:
-                acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_inflow[f'snap2_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap2_{dataset}']),dataset_shapes[dataset]))
-                acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_inflow[f'snap1_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap1_{dataset}']),dataset_shapes[dataset]))
-                acc_file[ihalo_group]['Outflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_outflow[f'snap2_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_outflow[f'snap2_{dataset}']),dataset_shapes[dataset]))
-                acc_file[ihalo_group]['Outflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_outflow[f'snap1_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_outflow[f'snap1_{dataset}']),dataset_shapes[dataset]))
+                acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_inflow[f'snap2_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap2_{dataset}']),dataset_shapes[dataset]),exact=False)
+                acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_inflow[f'snap1_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap1_{dataset}']),dataset_shapes[dataset]),exact=False)
+                acc_file[ihalo_group]['Outflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_outflow[f'snap2_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_outflow[f'snap2_{dataset}']),dataset_shapes[dataset]),exact=False)
+                acc_file[ihalo_group]['Outflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_outflow[f'snap1_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_outflow[f'snap1_{dataset}']),dataset_shapes[dataset]),exact=False)
             else:
                 acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_inflow[f'snap2_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap2_{dataset}']),))
                 acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_inflow[f'snap1_{dataset}'],dtype=np.float32,shape=(len(ihalo_datasets_inflow[f'snap1_{dataset}']),))
