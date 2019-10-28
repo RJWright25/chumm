0b20b72b4b51fd14993441714e6de7569d812cdc
Ruby Wright <21486778@student.uwa.edu.au>
2019-10-28 11:32:25 +0800

    Cleaning up file handling

--- AccretionTools.py
+++ AccretionTools.py
@@ -1448,6 +1448,10 @@ def add_gas_particle_data(base_halo_data,accdata_path,datasets=None):
                             ihalo_datasets_outflow[f'snap2_{dataset}'].append(np.nan)
 
         for dataset in datasets:
+                ihalo_datasets_inflow[f'snap2_{dataset}']=np.array(ihalo_datasets_inflow[f'snap2_{dataset}'],dtype=np.float32)
+                ihalo_datasets_inflow[f'snap1_{dataset}']=np.array(ihalo_datasets_inflow[f'snap1_{dataset}'],dtype=np.float32)
+                ihalo_datasets_outflow[f'snap2_{dataset}']=np.array(ihalo_datasets_outflow[f'snap2_{dataset}'],dtype=np.float32)
+                ihalo_datasets_outflow[f'snap1_{dataset}']=np.array(ihalo_datasets_outflow[f'snap1_{dataset}'],dtype=np.float32)
                 acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_inflow[f'snap2_{dataset}'])
                 acc_file[ihalo_group]['Inflow']['PartType0'].require_dataset(f'snap1_{dataset}',data=ihalo_datasets_inflow[f'snap1_{dataset}'])
                 acc_file[ihalo_group]['Outflow']['PartType0'].require_dataset(f'snap2_{dataset}',data=ihalo_datasets_outflow[f'snap2_{dataset}'])
