3e62635daaf69714ef1556ae45ed28b07ffe3290
Ruby Wright <21486778@student.uwa.edu.au>
2019-10-29 17:37:39 +0800

    Cleaning up file handling

--- AccretionTools.py
+++ AccretionTools.py
@@ -1688,8 +1688,8 @@ def get_particle_acc_data(directory,halo_index_list=None):
         partfields_in[str(itype)]=fields_in_itype
         partfields_out[str(itype)]=fields_out_itype
 
-    print('fields for inflow': partfields_in)
-    print('fields for outflow': partfields_out)
+    print('fields for inflow:', partfields_in)
+    print('fields for outflow:', partfields_out)
 
     particle_acc_data_in={f"PartType{itype}":{field: [[] for i in range(desired_num_halos)] for field in partfields_in[str(itype)]} for itype in parttypes}
     particle_acc_data_out={f"PartType{itype}":{field: [[] for i in range(desired_num_halos)] for field in partfields_out[str(itype)]} for itype in parttypes}
