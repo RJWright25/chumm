48b144c655526fc8f81907723493f573bb45beaf
Ruby Wright <21486778@student.uwa.edu.au>
2019-11-13 15:55:49 +0800

    Adding all particle type properties for accretion data

--- AccretionTools.py
+++ AccretionTools.py
@@ -651,6 +651,20 @@ def gen_accretion_data_fof_serial(base_halo_data,snap=None,halo_index_list=None,
             idhalo_s1=np.nan
             idhalo_s3=np.nan
 
+        #Record halo position and velocity
+        if ihalo_s1>=0:
+            halo_hdf5.attrs.create('snap1_com',data=[base_halo_data[snap1]['Xc'][ihalo_s1],base_halo_data[snap1]['Yc'][ihalo_s1],base_halo_data[snap1]['Zc'][ihalo_s1]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap1_v',data=[base_halo_data[snap1]['VXc'][ihalo_s1],base_halo_data[snap1]['VYc'][ihalo_s1],base_halo_data[snap1]['VZc'][ihalo_s1]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap1_R200',data=base_halo_data[snap1]['R_200crit'][ihalo_s1],dtype=np.float32)
+        if ihalo_s2>=0:
+            halo_hdf5.attrs.create('snap2_com',data=[base_halo_data[snap2]['Xc'][ihalo_s2],base_halo_data[snap2]['Yc'][ihalo_s2],base_halo_data[snap2]['Zc'][ihalo_s2]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap2_v',data=[base_halo_data[snap2]['VXc'][ihalo_s2],base_halo_data[snap2]['VYc'][ihalo_s2],base_halo_data[snap2]['VZc'][ihalo_s2]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap2_R200',data=base_halo_data[snap2]['R_200crit'][ihalo_s2],dtype=np.float32)
+        if ihalo_s3>=0:
+            halo_hdf5.attrs.create('snap3_com',data=[base_halo_data[snap3]['Xc'][ihalo_s3],base_halo_data[snap3]['Yc'][ihalo_s3],base_halo_data[snap3]['Zc'][ihalo_s3]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap3_v',data=[base_halo_data[snap3]['VXc'][ihalo_s3],base_halo_data[snap3]['VYc'][ihalo_s3],base_halo_data[snap3]['VZc'][ihalo_s3]],dtype=np.float32)
+            halo_hdf5.attrs.create('snap3_R200',data=base_halo_data[snap3]['R_200crit'][ihalo_s3],dtype=np.float32)
+        
         ihalo_tracked=(ihalo_s1>-1 and ihalo_s3>-1)#track if have both progenitor and descendant
         structuretype=base_halo_data[snap2]["Structuretype"][ihalo_s2]#structure type
 
