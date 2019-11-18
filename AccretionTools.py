606be7e93f16a5a1ccec699b8c2c7c4380c493f1
Ruby Wright <21486778@student.uwa.edu.au>
2019-11-18 13:55:02 +0800

    Adding function to find particle indices

--- AccretionTools.py
+++ AccretionTools.py
@@ -417,32 +417,32 @@ def get_particle_indices(base_halo_data,sorted_IDs,IDs,Types,snap_taken,snap_des
         parttypes=[0,1]
         search_types={'0':[0],'1':[1]}
 
-    Types_atsnap=np.zeros(npart)-1
-    Indices_atsnap=np.zeros(npart)-1
+    types_atsnap=np.zeros(npart)-1
+    indices_atsnap=np.zeros(npart)-1
 
     ipart=0
-    for ipart,ID,Type in zip(list(range(npart)),IDs,Types):
+    for ipart,ipart_id,ipart_type in zip(list(range(npart)),IDs,Types):
         #find new type
-        search_in=search_types[str(Type)]
+        search_in=search_types[str(ipart_type)]
         if len(search_in)==1:
-            out_Type=search_in[0]
+            out_type=search_in[0]
         else:
             for itype in search_in:
-                test_index=binary_search(items=[ID],sorted_list=sorted_IDs[f'PartType{itype}'],check_entries=True)[0]
+                test_index=binary_search(items=[ipart_id],sorted_list=sorted_IDs[f'PartType{itype}'],check_entries=True)[0]
                 if test_index>=0:
-                    out_Type=itype
+                    out_type=itype
                     break
                 else:
                     continue
-        Types_atsnap[ipart]=out_Type
+        types_atsnap[ipart]=out_type
 
     for itype in parttypes:
-        itype_mask=np.where(Types_atsnap==itype)
+        itype_mask=np.where(types_atsnap==itype)
         print(f'num of {itype}: ',np.sum(itype_mask))
         itype_indices=binary_search(items=np.array(IDs)[itype_mask],sorted_list=sorted_IDs[f'PartType{itype}'],check_entries=False)
-        Indices_atsnap[itype_mask]=itype_indices
+        indices_atsnap[itype_mask]=itype_indices
 
-    return Indices_atsnap.astype(int),Types_atsnap.astype(int)
+    return indices_atsnap.astype(int),types_atsnap.astype(int)
 
 
 ########################### GENERATE ACCRETION DATA ###########################
