6471243eea8e35d4efffe3c0f47b7fc57898d4a3
Ruby Wright <21486778@student.uwa.edu.au>
2019-11-18 14:11:33 +0800

    Adding function to find particle indices

--- AccretionTools.py
+++ AccretionTools.py
@@ -439,7 +439,6 @@ def get_particle_indices(base_halo_data,sorted_IDs,sorted_indices,IDs,Types,snap
 
     for itype in parttypes:
         itype_mask=np.where(parttypes_atsnap==itype)
-        print(f'num of {itype}: ',np.size(itype_mask))
         itype_indices=binary_search(items=np.array(IDs)[itype_mask],sorted_list=sorted_IDs[f'PartType{itype}'],check_entries=False)
         historyindices_atsnap[itype_mask]=itype_indices
     
