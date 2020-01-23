6217e0d75836c48d3da539486fe0d853f679f766
Ruby Wright <21486778@student.uwa.edu.au>
2020-01-23 15:53:24 +0800

    Improving memory usage

--- AccretionTools.py
+++ AccretionTools.py
@@ -490,7 +490,7 @@ def gen_accretion_data_eagle(base_halo_data,snap=None,halo_index_list=None,pre_d
                     ihalo_hdf5['Outflow'].create_group(f'PartType{itype}')        
         
         # This catches any exceptions for a given halo and prevents the code from crashing 
-        if True:     
+        try:     
             # try:
             ########################################################################################################################################
             ###################################################### ihalo PRE-PROCESSING ############################################################
@@ -973,7 +973,7 @@ def gen_accretion_data_eagle(base_halo_data,snap=None,halo_index_list=None,pre_d
                 progress_file.close()
         
         # Some other error in the main halo loop
-        else: 
+        except: 
             print(f'Skipping ihalo {ihalo_s2} - dont have the reason')
             with open(fname_log,"a") as progress_file:
                 progress_file.write(f"Skipping ihalo {ihalo_s2} - unknown reason ({iihalo+1} out of {num_halos_thisprocess} for this process - {(iihalo+1)/num_halos_thisprocess*100:.2f}% done)\n")
