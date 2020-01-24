a859d4a5b75f4dec9333ef55b8941b96b394aa97
Ruby Wright <21486778@student.uwa.edu.au>
2020-01-24 14:41:32 +0800

    Adding compatibility for no particles in parthistories

--- ParticleTools.py
+++ ParticleTools.py
@@ -243,13 +243,7 @@ def postprocess_particle_history_serial(base_halo_data,path='part_histories'):
         ##### Step 1: Transfer old processing level ###
         ###############################################
         if not (isnap==0 or isnap0_skipped):
-            if True:
-                iprev_itype_processing_count=[np.sum(iprev_itype_processing_level[str(itype)]>0) for itype in PartTypes]
-            # except:
-            #     print(f'Skipping snap {snap_abs} ...')
-            #     isnap0_skipped=True
-            #     continue
-
+            iprev_itype_processing_count=[np.sum(iprev_itype_processing_level[str(itype)]>0) for itype in PartTypes]
             iprev_all_processed_count=int(np.sum(iprev_itype_processing_count))
             iprev_all_processed_IDs=np.zeros(iprev_all_processed_count,dtype=np.int64)
             iprev_all_processed_Types=np.zeros(iprev_all_processed_count,dtype=np.int8)
@@ -260,6 +254,11 @@ def postprocess_particle_history_serial(base_halo_data,path='part_histories'):
             for iitype,itype in enumerate(PartTypes):
                 # Count number of previously processed particles of this type and assign indices
                 iprev_iitype_processing_count=iprev_itype_processing_count[iitype]
+                if iprev_iitype_processing_count==0:
+                    print(f'Skipping snap {snap_abs} ...')
+                    isnap0_skipped=True
+                    break
+                    
                 iprev_all_processed_index_end=iprev_all_processed_index_start+iprev_iitype_processing_count
 
                 # Get mask of previous particles which were processed
