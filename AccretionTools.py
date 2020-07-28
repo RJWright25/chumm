a35ff454be113e0d98a86fe519bf7e397a70d426
Ruby Wright <21486778@student.uwa.edu.au>
2020-07-28 19:20:31 +0800

    Adding h5struct function

--- AccretionTools.py
+++ AccretionTools.py
@@ -2713,7 +2713,7 @@ def postprocess_accretion_data_serial(base_halo_data,path=None):
 
     # Read in the hdf5 data structure of outputs
     allfnames=os.listdir(path)
-    accfnames=[path+fname for fname in allfnames if ('AccretionData' in fname and 'All' not in fname)]
+    accfnames=[path+fname for fname in allfnames if ('AccretionData' in fname and 'All' not in fname and '.hdf5' in fname)]
 
     integrated_datasets_list=np.array(hdf5_struct(accfnames[-1]))
     print(f'Total num datasets: {len(integrated_datasets_list)}')
